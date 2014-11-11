ce5839a2d29b7090dc7437e320b630c61489a332
Jack Martin <JM273606@gmail.com>
2014-11-11 15:34:21 -0500

    Updates

---
+++ ChromeDriverTest.py
@@ -0,0 +1,16 @@
+"""testing Selenium with ChromeDriver"""
+
+import time
+
+from selenium import webdriver
+
+
+
+driver = webdriver.Chrome('/Users/j/Downloads')  
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) 
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) 
+driver.quit()
